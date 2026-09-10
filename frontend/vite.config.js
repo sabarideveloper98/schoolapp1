7c6014e61123cf8d1c847876698d7d2c475a8e42
sabarisgit <k2b.sabari@gmail.com>
2026-09-10 13:14:31 +0530

    chore: update development proxy target to local server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.school1.in',
+        target: 'http://localhost:5005',
+        // target: 'https://api.school1.in',
         changeOrigin: true,
       },
     },
